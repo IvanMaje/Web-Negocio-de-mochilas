7f7a234a8653d787f3beff320d4c2065f4264bb7
IvanMajewski <108892887+IvanMajewski@users.noreply.github.com>
2022-09-30 21:15:12 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const app = express();
 require('./database');
 
 //Settings
-app.set('port', process.env.PORT || 4000);
+app.set('port', process.env.PORT || 6000);
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(expressLayouts);
